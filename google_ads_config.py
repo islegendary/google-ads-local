70f5c9ec80534f938dd1c8be3d41236689a6e632
islegendary <88680927+islegendary@users.noreply.github.com>
2025-06-13 17:52:01 -0400

    Create google_ads_config.py

---
+++ google_ads_config.py
@@ -0,0 +1,10 @@
+# google_ads_config.py
+GOOGLE_ADS_CONFIG = {
+    "developer_token": "",
+    "login_customer_id": "",  # your manager account, no hyphens
+    "client_id": "",
+    "client_secret": "",
+    "refresh_token": "",
+    "use_proto_plus": True,
+    "access_token": ""
+}
